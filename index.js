cb4c4c198c6a6a5c0e32c8caf192678943fe8455
Kenneth Mei <meikenneth321@gmail.com>
2022-04-27 00:17:51 -0400

    commit tree 5dc2b9b001cddb400dea14e07c0b1a4315f96fe6

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = 'BOB'
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 9;
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 10;
+}
